3868f5c3bc7ee85a4def5e0e6cd2e01ec4249148
IngDeiver <deiver.guerra.2017@upb.edu.co>
2020-11-07 21:13:58 -0500

    Animaciones al caminar (solo a la derecha)

--- Quedate en casa pacman/Assets/Scripts/MoviemientoPacman.cs
+++ Quedate en casa pacman/Assets/Scripts/MoviemientoPacman.cs
@@ -12,17 +12,25 @@ public class MoviemientoPacman : MonoBehaviour
     private bool up;
     private bool down;
     float delta = 3f;
+    private Animator animator;
 
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+
+        up = false;
+        down = false;
+        left = false;
+        right = false;
+        animator.SetBool("caminando", false);
+
 
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
@@ -34,7 +42,9 @@ public class MoviemientoPacman : MonoBehaviour
             down = false;
             left = true;
             right = false;
+            animator.SetBool("caminando", true);
         }
+
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             Vector3 position = this.transform.position;
@@ -45,7 +55,9 @@ public class MoviemientoPacman : MonoBehaviour
             down = false;
             left = false;
             right = true;
+            animator.SetBool("caminando", true);
         }
+
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             Vector3 position = this.transform.position;
@@ -56,7 +68,9 @@ public class MoviemientoPacman : MonoBehaviour
             down = false;
             left = false;
             right = false;
+            animator.SetBool("caminando", true);
         }
+
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             Vector3 position = this.transform.position;
@@ -67,7 +81,9 @@ public class MoviemientoPacman : MonoBehaviour
             down = true;
             left = false;
             right = false;
+            animator.SetBool("caminando", true);
         }
+
 
     }
 
