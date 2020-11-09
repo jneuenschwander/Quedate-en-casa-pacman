c9614d6e12582cb1691720c19f87d28fc2f62c68
Juan Antonio <41336919+jneuenschwander@users.noreply.github.com>
2020-11-08 22:13:37 -0500

    add player 2

---
+++ Quedate en casa pacman/Assets/Scripts/MovimientoCovid.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoCovid : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Vector3 position = this.transform.position;
+            position.x -= 0.3f;
+            this.transform.position = position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Vector3 position = this.transform.position;
+            position.x += 0.3f;
+            this.transform.position = position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+
+            Vector3 position = this.transform.position;
+            position.y += 0.3f;
+            this.transform.position = position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 position = this.transform.position;
+            position.y -= 0.3f;
+            this.transform.position = position;
+        }
+   }
+}
